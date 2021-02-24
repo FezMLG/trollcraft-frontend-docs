2a9c86ba284acaf18eb9801e2bdd587c10f6f42b
FezMLG <46359181+FezMLG@users.noreply.github.com>
2021-02-24 18:05:19 +0100

    add nprogress to page

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,12 @@
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
+
 import '../styles/global.scss'
 import '../styles/components.scss'
 
